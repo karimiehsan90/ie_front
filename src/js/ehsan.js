f52f4c5548f60a8bfcce690a7422db4fa817c99a
ehsan karimi <karimiehsan901@gmail.com>
2019-04-18 16:10:10 +0430

    my_cases add_case users

--- src/js/ehsan.js
+++ src/js/ehsan.js
@@ -8,8 +8,26 @@ $(".close-side").on("click", function () {
 
 $(".fa-filter").on("click", function () {
     $(".spinner-border").removeClass("d-none");
-    $.get("https://jsonplaceholder.typicode.com/users/1", function (data) {
-
+    $.get("../api/admin-cases.json", function (data) {
+        alert(data.data[0].title);
     });
     $(".spinner-border").addClass("d-none");
+});
+
+$("#accept").on("click", function () {
+    $(this).removeClass("btn-bg-shadow");
+    $("#manage").removeClass("btn-bg-stone");
+    $("#manage").addClass("btn-bg-shadow");
+    $(this).addClass("btn-bg-stone");
+    $("#manage_users").addClass("d-none");
+    $("#accept_users").removeClass("d-none");
+});
+
+$("#manage").on("click", function () {
+    $(this).removeClass("btn-bg-shadow");
+    $("#accept").removeClass("btn-bg-stone");
+    $("#accept").addClass("btn-bg-shadow");
+    $(this).addClass("btn-bg-stone");
+    $("#manage_users").removeClass("d-none");
+    $("#accept_users").addClass("d-none");
 });
